208d756598f679bb1a507102a372c352ca321ddb
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-05 17:30:44 +0100

    Add HeaderLayout component.

---
+++ src/Components/HeaderLayout/HeaderLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeaderLayout.css';
+import BackgroundBox from '../BackgroundBox/BackgroundBox';
+
+const Navbar = () => {
+  return (
+    <div className="top-fixed-panel">
+      <BackgroundBox />
+    </div>
+  );
+};
+
+export default Navbar;
