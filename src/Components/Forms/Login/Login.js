eee4a8dcf294cf99e93b574943e1d2bf3c536850
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-07 15:30:17 +0100

    Create Form component for Login

---
+++ src/Components/Forms/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './LoginForm';
+
+const Login = () => {
+  return (
+    <Form
+      FormTitle="Login"
+      formMessage="Don't have an account yet? SignUp Here."
+    />
+  );
+};
+
+export default Login;
