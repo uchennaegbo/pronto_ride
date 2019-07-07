06167787ee928f090a3c013c8f56900b5cde7035
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-07 15:31:44 +0100

    Add Login Form event handlers components

---
+++ src/Components/Forms/Login/useForm.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (callback, validate) => {
+  const [values, setValues] = useState({ name: '', email: '', password: '' });
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    setValues({
+      ...values,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setErrors(validate(values));
+    setIsSubmitting(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [callback, errors, isSubmitting]);
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    errors
+  };
+};
+
+export default useForm;
