8cb2ebe5fbe5fe0a77f5c3662079e8d134a7d5ff
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-30 13:08:55 +0100

    [Component]: Add signUp form.

---
+++ src/Components/Forms/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './SignUpForm';
+
+const SignUp = () => {
+  return (
+    <Form
+      FormTitle="Sign Up"
+      formMessage="Already have an account? Login Here."
+    />
+  );
+};
+
+export default SignUp;
