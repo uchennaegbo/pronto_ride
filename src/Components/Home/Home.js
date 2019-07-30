ed6f4e9282b03843faad04dd000265400090fd79
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-07-30 20:28:09 +0100

    Add Home component.

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import Navbar from '../Navbar/Navbar';
+import HeaderLayout from '../HeaderLayout/HeaderLayout';
+import SectionContent from '../Section/SectionContent';
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <HeaderLayout />
+      <SectionContent />
+    </>
+  );
+};
+
+export default Home;
